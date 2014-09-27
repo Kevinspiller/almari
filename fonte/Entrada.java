f1c92ee580d5e3c4be300dd50f46a7abe545d0cc
maico-smaniotto <maicosmaniotto@yahoo.com.br>
2014-09-27 02:01:05 -0300

    - ajuste nos nomes dos métodos da classe "Entrada" - incluído método para leitura de palavra

--- fonte/Entrada.java
+++ fonte/Entrada.java
@@ -1,37 +1,44 @@
-Import java.util.Scanner
+import java.util.Scanner;
 
-class Entrada{
+class Entrada {
 	
-	public string leitura_Linha(){           //Faz a leitura da linha total, salvando em uma string e retornando a mesma
+	public String leLinha(){           //Faz a leitura da linha total, salvando em uma string e retornando a mesma
 		Scanner S;
-		S = new Scanner(system.in);
+		S = new Scanner(System.in);
 		String a = S.nextLine();
 		return a;
 	}
 	
-	public int leitura_Int(){				  //Faz a leitura de um int, salvando em um int i e retornando o mesmo
+	public String lePalavra() {
 		Scanner S;
-		S = new Scanner(system.in);
+		S = new Scanner(System.in);
+		String a = S.next();
+		return a;
+	}
+	
+	public int leInteiro(){				  //Faz a leitura de um int, salvando em um int i e retornando o mesmo
+		Scanner S;
+		S = new Scanner(System.in);
 		int i = S.nextInt();
 		S.nextLine();							// como o professor comentou, a leitura de int ou double deixa um /n, por isso essa leitura de linha
 		return i;
 	}
 	
-	public double leitura_Double(){			  //Faz a leitura de um double, salvando em um double d e retornando o mesmo
+	public double leReal(){			  //Faz a leitura de um double, salvando em um double d e retornando o mesmo
 		Scanner S;
-		S = new Scanner(system.in);
-		double d = S.nextInt();
+		S = new Scanner(System.in);
+		double d = S.nextDouble();
 		S.nextLine();
 		return d;
 	}
 	
-	public char leitura_char(){				//Faz a leitura de um char, salvando em um char c e retornando o mesmo
-		Scanner S;
-		S = new Scanner(system.in);
-		char c = S.nextInt();
-		S.nextLine();						// não tenho certeza se no char é preciso fazer isso também
-		return c;
-	}
+//	public char leCaractere(){				//Faz a leitura de um char, salvando em um char c e retornando o mesmo
+//		Scanner S;
+//		S = new Scanner(System.in);
+//		char c = S.nextInt();
+//		S.nextLine();						// não tenho certeza se no char é preciso fazer isso também
+//		return c;
+//	}
 }
 	
 	//Olhem o email que mandei, pois fiquei com aquela duvida, e mais, tem que fazer o float e o long int/long long ???
