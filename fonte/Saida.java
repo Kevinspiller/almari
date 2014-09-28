c0e65349724d57467825793a0bcc4550bdb47033
maico-smaniotto <maicosmaniotto@yahoo.com.br>
2014-09-27 22:22:44 -0300

    - ajustes na classe Saida, passando a utilizar os métodos para buscar variáveis e verificar se o caractere é um símbolo ou espaço em branco

--- fonte/Saida.java
+++ fonte/Saida.java
@@ -1,31 +1,51 @@
-class Saida{
-	
-	public void Imprimir (String I[]){
-		private int a=0;
-		while(a<I.length){                                          // vai rodar o While enquanto o a não for maior que o tamanho do vetor
-			if(I[a] == ' " '){                                		//Testa se aquela parte do vetor contem aspas 
-				for(a++; I[a] != ' " ' && a<I.length; a++){ 		//vai imprimindo tudo que esta entre as aspas, até achar uma outra aspa
-				System.out.print(I[a]);								
-				}						//	
-			}else if( (I[a] >= 'a' && I[a] <='z') || (I[a]>= 'A' && I[a] <= 'Z'){   //testa se o caractere é uma letra, que caracteriza o inicio do nome da variavel 
-				private Char nomVar[];												// seria a criação de um vetor char, para salvar esses caracteres
-				private int i=0;														
-				private nomVar[i];
-				for(;I[a] != ' ' && a<I.length; a++, i++){                         //aqui ele salva todos os caracteres seguintes, até o espaço, que seria o nome da variavel																		
-					nomVar[i]=I[a];													//aqui apenas salva esses caracteres na nomVar					
+class Saida {
+
+	public void imprime(Escopo escopo, String linha){
+		int i = 0, j;
+		while (i < linha.length()) {                                          // vai rodar o While enquanto o a não for maior que o tamanho do vetor
+			if (linha.charAt(i) == '"') { //Testa se aquela parte do vetor (token) contém aspas 
+				for (i++; linha.charAt(i) != '"' && i < linha.length(); i++) { 		//vai imprimindo tudo que esta entre as aspas, até achar uma outra aspa
+					System.out.print(linha.charAt(i));
 				}
-				System.out.print (funcao_Que_procura_variavel(nomVar[i+1]) )	// passa como parâmetro a string com o nome da variavel para outra função, imprimindo ela direto
-			}else if(I[a] == ' '){												//aqui é se encontrar um espaço, para desconsiderar e avançar uma posição
-				a++;
+			} else if (Verificador.charValidoNome(linha.charAt(i))) {   //testa se o caractere é uma letra, que caracteriza o inicio do nome da variavel 
+				String nomVar = "";												// seria a criação de um vetor char, para salvar esses caracteres
+				j = 0;
+
+				for (; (! Verificador.ehEspacoEmBranco(linha.charAt(i))) && (! Verificador.ehSimbolo(linha.charAt(i))) && i < linha.length(); i++, j++) {                         //aqui ele salva todos os caracteres seguintes, até o espaço, que seria o nome da variavel																		
+					nomVar += linha.charAt(i);													//aqui apenas salva esses caracteres na nomVar					
+				}
+
+				Variavel var = escopo.buscaVariavel(nomVar); // passa como parâmetro a string com o nome da variavel para outro método
+				if (var != null) {
+					switch (var.getTipo()) {
+						case 'I': 
+							System.out.print(((Inteiro)var).getValor());
+							break;
+						case 'R':
+							System.out.print(((Real)var).getValor());
+							break;
+						case 'C': 
+							System.out.print(((Caractere)var).getValor());
+							break;
+					}
+				} else {
+					// não encontrou variável com esse nome, verifica se é um número, tentando converter para double
+					try {
+						System.out.print(Double.parseDouble(nomVar));
+					} catch (NumberFormatException e) {
+						System.out.print("\nVariável " + nomVar + " não declarada");
+					}
+				}
+			} else if (Verificador.ehEspacoEmBranco(linha.charAt(i))) {
+				i++;
 			}
 		}
 	}
 	
-	public void Imprimir_Linha(String I[]){                       //Função igual a imprimir, mas que imprime e da um /n no final
-		this.imprimir(I[]);									//chama a outra função, pois faz a mesma coisa
-		system.out.print(\n);								// da o \n
+	public void imprimeLinha(Escopo escopo, String linha){ // Método igual ao imprime, mas que imprime e da um /n no final
+		this.imprime(escopo, linha);						 // chama o outro método, pois faz a mesma coisa
+		System.out.print("\n");							 // da o \n
 	}
-	
 }
 	
 	
