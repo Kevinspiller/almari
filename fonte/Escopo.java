0f8f46557698819d0a70b6bbb0f337f7c5c16098
maico-smaniotto <maicosmaniotto@yahoo.com.br>
2014-09-28 17:37:31 -0300

    - ajustes na classe Escopo: * alterado o método declaraVariavel, passando a tratar a declaração de variável sem atribuição de valor inicial, para assumir zero nos tipos inteiro e real * ajuste na identificação dos comandos de declaração de variável, passando a tratar se o próximo token existe
    - renomeada a classe Main para Interpretador

--- fonte/Escopo.java
+++ fonte/Escopo.java
@@ -13,11 +13,13 @@ class Escopo {
 		Variavel var = null;
 		if (! existeVariavel(nome)) {
 			if (tipo.equals("inteiro")) {
-				var = new Inteiro(nome, Integer.parseInt(valor));
+				var = new Inteiro(nome, valor.equals("") ? 0 : Integer.parseInt(valor));
 			} else if (tipo.equals("real")) {
-				var = new Real(nome, Double.parseDouble(valor));
+				var = new Real(nome, valor.equals("") ? 0.0 : Double.parseDouble(valor));
 			} else if (tipo.equals("caractere")) {
 				var = new Caractere(nome, valor);
+			} else {
+				throw new IllegalArgumentException("Tipo inválido: " + tipo);
 			}
 		}
 		return var;
@@ -50,7 +52,6 @@ class Escopo {
 		for (int i = 0; i < variaveis.size(); i++) {
 			this.vars.add(variaveis.get(i));
 		}
-		processa(); // ver se não é melhor chamar este método de fora
 	}
 	
 	private void processa() throws IllegalArgumentException {
@@ -59,44 +60,46 @@ class Escopo {
 	
 		for (i = 0; i < this.comandos.length(); i++) {
 			j = 0;
-			buffer = buffer + comandos.charAt(i+j);
+			buffer = buffer + comandos.charAt(i + j);
 			while (fimComando(buffer.charAt(j)))  { //aqui alimentamos o buffer até achar um fim de comando
 				j++;
-				buffer = buffer + comandos.charAt(i+j);
+				buffer = buffer + comandos.charAt(i + j);
 			}
 			
 			//////////////////////////////////////////
 			//////////////////////////////////////////
-			String[] tokens = buffer.split(" +|\t+|\n+");
+			String[] tokens = buffer.split(" +|\t+|\n+|\r+");
 
-			// tratamento da declaração de variáveis e funções
-			// falta verificar se as posições do vetor existem antes de tentar acessá-las
+			// tratamento da declaração de variáveis
+			// funções são tratadas na classe Interpretador pois ficam declaradas fora do escopo
 			if (tokens[0].equals("var")) {
-				if (Verificacao.tipoValido(tokens[1])) {
-					if (Verificacao.nomeValido(tokens[2])) {
-						if (tokens[3].equals(":=")) {
+				if (tokens.length >= 2 && Verificacao.tipoValido(tokens[1])) {
+					if (tokens.length >= 3 && Verificacao.nomeValido(tokens[2])) {
+						if (tokens.length >= 4 && tokens[3].equals(":=")) {
 							// declaração com atribuição, verifica se o valor que está atribuindo corresponde ao tipo declarado
-							if (Verificacao.valorValido(tokens[1], tokens[4])) {
+							if (tokens.length >= 5 && Verificacao.valorValido(tokens[1], tokens[4])) {
 								// declara passando o tipo, nome e valor
 								declaraVariavel(tokens[1], tokens[2], tokens[4]);
 							} else {
-								throw new IllegalArgumentException("O valor " + tokens[4] + " não é válido para a variável " + tokens[2] + " do tipo " + tokens[1]);
+								throw new IllegalArgumentException("Esperado valor válido para a variável " + tokens[2] + " do tipo " + tokens[1]);
 							}
 						} else {
 							declaraVariavel(tokens[0], tokens[2], "");
-							if (tokens[4].equals(",")) {
-								// essa parte se vamos permitir declaração de múltiplas variáveis separadas por vírgula
-							} else if (! tokens[4].equals(";")) {
-								throw new IllegalArgumentException("Símbolo " + tokens[4] + " inválido. Esperado , ou ;");
+							if (tokens.length >= 5) {
+								if (tokens[4].equals(",")) {
+									// essa parte se vamos permitir declaração de múltiplas variáveis separadas por vírgula
+								} else if (! tokens[4].equals(";")) {
+									throw new IllegalArgumentException("Símbolo " + tokens[4] + " inválido. Esperado , ou ;");
+								}
 							}
 						}
 					} else {
-						throw new IllegalArgumentException("Nome de variável inválido: " + tokens[2]);
+						throw new IllegalArgumentException("Esperado nome de variavel apos tipo");
 					}
 				} else {
-					throw new IllegalArgumentException("Tipo de variável inválido: " + tokens[1]);
+					throw new IllegalArgumentException("Esperado tipo de variável apos var");
 				}
-			} else if (tokens[0].equals("func")) {
+			} /* else if (tokens[0].equals("func")) {
 				if (Verificacao.tipoValido(tokens[1])) {
 					if (Verificacao.nomeValido(tokens[2])) {
 						// declara a função
@@ -107,13 +110,14 @@ class Escopo {
 					throw new IllegalArgumentException("Tipo de função inválido: " + tokens[j + 1]);
 				}
 			}
+			*/
 			/////////////////////////////////////
 			/////////////////////////////////////
 
 
 		
 			if (buffer.charAt(j) == '{') { //se o fim de comando foi o "{", então são quatro possibilidades, while, if, função ou erro
-				for (l = 0; buffer.charAt(l) != ' ' && buffer.charAt(l) != '\t' && buffer.charAt(l) != '\n'; l++); //aqui eliminamos os primeiros espaços, TAB ou \n (sintaxe flexivel)
+				for (l = 0; !Character.isWhitespace(buffer.charAt(l)); l++); //aqui eliminamos os primeiros espaços, TAB ou \n (sintaxe flexivel)
 				h = 0;
 				while (buffer.charAt(l) != ' ' && buffer.charAt(l) != '{' && buffer.charAt(l) != '(') { //por ser sintaxe flexivel, os parenteses podem estar colados no comando
 					instruct = instruct + buffer.charAt(l); //aqui alimentamos a instrução (while, função, if ou erro)
